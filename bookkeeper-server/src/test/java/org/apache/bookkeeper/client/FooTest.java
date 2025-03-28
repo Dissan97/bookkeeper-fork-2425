0fa2b067640cdc75a5ae20051a98c552a1317392
dissan-ahmed <dissanahmed@gmail.com>
2025-03-28 10:11:38 +0100

    Added CI&CT workflow

---
+++ bookkeeper-server/src/test/java/org/apache/bookkeeper/client/FooTest.java
@@ -0,0 +1,15 @@
+package org.apache.bookkeeper.client;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class FooTest {
+
+
+    @Test
+    public void testFoo() {
+        BookKeeper bookKeeper = new BookKeeper();
+        assertTrue(true);
+    }
+}
